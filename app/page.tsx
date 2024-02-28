d9cbc561811649fa50706467d85d94486ad2c82b
varunpats <varunpatil2502@gmail.com>
2024-02-28 10:45:38 +0530

    Designed About section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import About from "@/components/about";
+import Divider from "@/components/divider";
 import Intro from "@/components/intro";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center">
       <Intro />
+      <Divider />
+      <About />
     </main>
   )
 }
--- components/intro.tsx
+++ components/intro.tsx
@@ -38,7 +38,7 @@ export default function Intro() {
                 </div>
             </div>
 
-            <motion.p className='mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl'
+            <motion.h1 className='mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl'
                 initial={{ opacity: 0, y: 100 }}
                 animate={{ opacity: 1, y: 0 }}
             >
@@ -46,8 +46,8 @@ export default function Intro() {
                 <span className="font-bold">front-end developer</span> with{" "}
                 <span className="font-bold">4+ years</span> of experience. I enjoy
                 building <span className="italic">sites & apps</span>. My focus is{" "}
-                <span className="underline">React (Next.js)</span>.
-            </motion.p>
+                <span className="underline">React</span>.
+            </motion.h1>
 
             <motion.div className='flex flex-col sm:flex-row items-center justify-center gap-3 px-4 text-lg font-medium'
                 initial={{ opacity: 0, y: 100 }}
