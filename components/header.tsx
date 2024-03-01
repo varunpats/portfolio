1f798f4d6cb99521ac8161d51c7c4e83eeb92ca0
varunpats <varunpatil2502@gmail.com>
2024-03-01 11:44:56 +0530

    Started with header section

--- components/header.tsx
+++ components/header.tsx
@@ -1,9 +1,14 @@
-'use client'
+'use client';
+
 import Link from "next/link";
 import { motion } from "framer-motion";
 import { links } from "@/lib/data";
+import { useState } from "react";
+import clsx from "clsx";
 
 export default function Header() {
+    const [activeSection, setActiveSection] = useState('Home');
+
     return <header className="z-[999] relative">
         <motion.div
             className="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75"
@@ -21,21 +26,21 @@ export default function Header() {
                         animate={{ y: 0, opacity: 1 }}
                     >
                         <Link
-                            className={"flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300"
-                                // {
-                                //     "text-gray-950 dark:text-gray-200":
-                                //         activeSection === link.name,
-                                // }
-                            }
+                            className={clsx("flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300",
+                                {
+                                    "text-gray-950 dark:text-gray-200":
+                                        activeSection === link.name,
+                                }
+                            )}
                             href={link.hash}
-                            // onClick={() => {
-                            //     setActiveSection(link.name);
-                            //     setTimeOfLastClick(Date.now());
-                            // }}
+                            onClick={() => {
+                                setActiveSection(link.name);
+                                // setTimeOfLastClick(Date.now());
+                            }}
                         >
                             {link.name}
 
-                            {/* {link.name === activeSection && (
+                            {link.name === activeSection && (
                                 <motion.span
                                     className="bg-gray-100 rounded-full absolute inset-0 -z-10 dark:bg-gray-800"
                                     layoutId="activeSection"
@@ -45,7 +50,7 @@ export default function Header() {
                                         damping: 30,
                                     }}
                                 ></motion.span>
-                            )} */}
+                            )}
                         </Link>
                     </motion.li>
                 ))}
