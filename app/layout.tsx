43e5e82e0ce74e798805f6cc1c18f4678236d6a9
varunpats <varunpatil2502@gmail.com>
2024-03-13 11:08:48 +0530

    Designed footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 import ActiveSectionContext from '@/context/active-section-context'
 import { Toaster } from 'react-hot-toast'
+import Footer from '@/components/footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -24,6 +25,7 @@ export default function RootLayout({
         <ActiveSectionContext>
           <Header />
           {children}
+          <Footer />
           <Toaster position='top-right' />
         </ActiveSectionContext>
       </body>
---
+++ components/footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer className='mb-6 px-4 text-center text-gray-500'>
+        <small className='text-xs'>
+            &copy; 2024 Varun. All rights reserved.
+        </small>
+    </footer>
+  )
+}
