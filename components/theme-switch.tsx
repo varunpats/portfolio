1e746cea597eabf2b5c793f0bbed30e988d8b43b
varunpats <varunpatil2502@gmail.com>
2024-03-19 23:55:10 +0530

    Refactored theme code

--- components/theme-switch.tsx
+++ components/theme-switch.tsx
@@ -1,34 +1,10 @@
 "use client"
 
-import React, { useEffect, useState } from 'react'
+import { useTheme } from '@/context/theme-context';
 import { BsMoon, BsSun } from 'react-icons/bs';
 
-type Theme = "light" | "dark";
-
 export default function ThemeSwitch() {
-    const [theme, setTheme] = useState<Theme>("light");
-
-    useEffect(() => {
-        const localTheme = window.localStorage.getItem('theme') as Theme | null;
-
-        if (localTheme) {
-            setTheme(localTheme);
-            if (localTheme === 'dark')
-                document.documentElement.classList.add("dark");
-        }
-    }, [])
-
-    const toggleTheme = () => {
-        if (theme === 'light') {
-            setTheme('dark')
-            window.localStorage.setItem('theme', 'dark')
-            document.documentElement.classList.add("dark");
-        } else {
-            setTheme('light')
-            window.localStorage.setItem('theme', 'light')
-            document.documentElement.classList.remove("dark");
-        }
-    }
+    const { theme, toggleTheme } = useTheme();
 
     return (
         <button className='fixed bottom-5 right-5 bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrrop-blur-[0.5rem] border border-white border-opacity-40 shadow-2xl flex items-center justify-center rounded-full hover:scale-[1.15] active:scale-105 transition-all dark:bg-gray-950' onClick={toggleTheme}>
