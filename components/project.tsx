f9e85963c11b59a7b600bacef2a227aa2d4f36e7
varunpats <varunpatil2502@gmail.com>
2024-03-20 00:12:34 +0530

    Made changes for responsive screens

--- components/project.tsx
+++ components/project.tsx
@@ -27,8 +27,8 @@ export default function Project({ title, description, tags, imageUrl }: ProjectP
         }}
         className='group mb-3 sm:mb-8 last:mb-0 even:'
     >
-        <section className='relative bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 sm:h-[20rem] hover:bg-gray-200 transition group-even:pl-8 dark:bg-white/10 dark:hover:bg-white/20 dark:text-white'>
-            <div className='flex flex-col pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] h-full group-even:ml-[18rem]'>
+        <section className='relative bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 sm:h-[20rem] hover:bg-gray-200 transition sm:group-even:pl-8 dark:bg-white/10 dark:hover:bg-white/20 dark:text-white'>
+            <div className='flex flex-col pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] h-full sm:group-even:ml-[18rem]'>
                 <h3 className='text-2xl font-semibold'>{title}</h3>
                 <p className='mt-2 leading-relaxed text-gray-700 dark:text-white/70'>{description}</p>
                 <ul className='flex flex-wrap mt-4 gap-2 sm:mt-auto'>
@@ -37,7 +37,7 @@ export default function Project({ title, description, tags, imageUrl }: ProjectP
                     ))}
                 </ul>
             </div>
-            <Image src={imageUrl} alt={title} quality={95} className='absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl group-hover:-translate-x-3 group-hover:translate-y-3 group-hover:-rotate-2 group-hover:scale-[1.05] group-even:right-[initial] group-even:-left-40 group-even:group-hover:-translate-x-3 group-even:group-hover:translate-y-3 group-even:group-hover:rotate-2 transition' />
+            <Image src={imageUrl} alt={title} quality={95} className='absolute hidden sm:block top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl group-hover:-translate-x-3 group-hover:translate-y-3 group-hover:-rotate-2 group-hover:scale-[1.05] group-even:right-[initial] group-even:-left-40 group-even:group-hover:-translate-x-3 group-even:group-hover:translate-y-3 group-even:group-hover:rotate-2 transition' />
         </section>
     </motion.div>
 }
