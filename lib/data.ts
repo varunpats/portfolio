d5696a82be84cc29fa0ed4c0915f87b93c71cf8b
varunpats <varunpatil2502@gmail.com>
2024-07-24 20:09:34 +0530

    Updated data.ts

--- lib/data.ts
+++ lib/data.ts
@@ -79,7 +79,7 @@ export const projectsData = [
         title: "React Dashboard",
         description:
             "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
-        tags: ["React", "Next.js"],
+        tags: ["React", "Recharts", "Scss", "react circular progressbar", "MUI"],
         imageUrl: wordanalyticsImg,
     },
 ] as const;
